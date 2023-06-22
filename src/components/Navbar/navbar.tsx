12ca77ee7193c017f15e260942de62c5cf219125
Guilherme-Pinto-Almeida <109819841+Guilherme-Pinto-Almeida@users.noreply.github.com>
2023-06-22 00:45:05 -0300

    import da lib geist + componentização

---
+++ src/components/Navbar/navbar.tsx
@@ -0,0 +1,29 @@
+import styles from '@/styles/Home.module.css'
+import { Inter } from 'next/font/google'
+import Server from '@geist-ui/icons/server'
+import { Text, Link } from "@geist-ui/core"
+
+const inter = Inter({ subsets: ['latin'] })
+
+const Navbar = () => {
+
+  return (
+    <main className={` ${inter.className}`}>	
+    <div className={styles.navbar}>
+    <div style={{display:'flex', alignItems:'center'}}>
+        <Link href='/'>
+            <Text b i font="30px" style={{ letterSpacing: '0.6px'  }}>
+                PIR
+            </Text>
+        </Link>
+        <Link href='/about'>
+            <Text marginLeft={2}> Sobre Nós</Text>
+        </Link>
+    </div>
+        <Text > Lorem Ipsum</Text>
+    </div>
+    </main>
+  );
+};
+
+export default Navbar;
