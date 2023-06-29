ffbbade365d9b2ceac02a3bdbc7b3781532928ae
pedrosalucas <54561127+pedrosalucas@users.noreply.github.com>
2023-06-28 21:00:35 -0300

    fix fix

--- src/pages/pergunta3/index.tsx
+++ src/pages/pergunta3/index.tsx
@@ -12,14 +12,6 @@ export default function Pergunta3() {
 	const [options, setOptions] = useState([]);
 	const [bancoSelected, setBancoSelected] = useState("");
 
-	//função alterada visando a correção do onChange dentro do <Select/>
-	const handleChange = (newValue: any| null) => {
-		if (newValue !== null) {
-			const { value } = newValue as { value: string; label: string };
-			setBancoSelected(value);
-		}
-	};
-
 	useEffect(() => {
 		async function bancosPossiveis() {
 			const bancos: any = await populaBancos();
@@ -38,6 +30,28 @@ export default function Pergunta3() {
 		if (bancoSelected !== "")
 			queryPergunta3(bancoSelected) ;
 	}, [bancoSelected]);
+
+	//função alterada visando a correção do onChange dentro do <Select/>
+	const handleChange = (newValue: any| null) => {
+		if (newValue !== null) {
+			const { value } = newValue as { value: string; label: string };
+			setBancoSelected(value);
+		}
+	};
+
+	const inst_name = () => {
+		if (data !== undefined && data.length !== 0) 
+			return `O ${data[0].nm_instituicao_financeira} tem o total de:`
+		
+		return "";
+	}
+
+	const complaint_amount = () => {
+		if (data !== undefined && data.length !== 0) 
+			return `${data[0].qtd_total_reclamacoes} reclamações`
+		
+		return "";
+	}
 
 	return (
 		<main className={styles.wrapper}>
@@ -72,12 +86,8 @@ export default function Pergunta3() {
 
 						<div>
 							<Spacer y={1} />
-							<h1>
-							{data !== undefined ? `O ${data[0].nm_instituicao_financeira} tem o total de:` : ""}
-							</h1>
-							<h2>
-							{data !== undefined ? `${data[0].qtd_total_reclamacoes} reclamações` : ""}
-							</h2>
+							<h1>{inst_name()}</h1>
+							<h2>{complaint_amount()}</h2>
 						</div>
 						</div>
 					</main>
