234f350f4f9041b0876a7ecd514256ae71333cb1
G4bzz <gabriel.god360@gmail.com>
2023-08-01 23:47:39 -0300

    Adjust on AccessInfo API

--- src/pages/api/accessInfo/index.ts
+++ src/pages/api/accessInfo/index.ts
@@ -25,7 +25,7 @@ export default async function handler(
       res.status(200).json(data);
       break;
     default:
-      res.status(405).json([{ message: "Method not allowed." }]);
+      res.status(405).json([]);
       break;
   }
 }
