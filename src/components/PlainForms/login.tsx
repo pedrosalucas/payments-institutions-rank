5e4c1b99e845ae5b6f22eec03d91e688f88bc371
pedrosalucas <54561127+pedrosalucas@users.noreply.github.com>
2023-08-06 09:56:31 -0300

    Add tag form in plain form login

--- src/components/PlainForms/login.tsx
+++ src/components/PlainForms/login.tsx
@@ -1,9 +1,8 @@
 import React, { ChangeEvent, useState } from "react";
 import { useRouter, useSearchParams } from "next/navigation";
 import { signIn } from "next-auth/react";
-import { Button } from "@geist-ui/core";
-import styles from '@/styles/Perguntas.module.css'
-import {FormElement, Input} from '@nextui-org/react'
+import { Button, FormElement, Input, Spacer } from '@nextui-org/react';
+import styles from '@/styles/Auth.module.css';
 
 const LoginForm = () => {
   const router = useRouter();
@@ -48,31 +47,40 @@ const LoginForm = () => {
   }
 
   return (
-    <div className={styles.flexmid}>
-      <div className={styles.signin}>
-        <Input
-          required
-          type="email"
-          name="email"
-          value={formValue.email}
-          onChange={handleChange}
-          clearable
-          label="Email"
-          placeholder="Seu email...."
-          size="xl" width="100%"
-        />
-        <Input.Password
-          required
-          type="password"
-          name="password"
-          value={formValue.password}
-          onChange={handleChange}
-          label="Senha"
-          placeholder="Sua Senha...."
-          size="xl" width="100%"
-        />
+    <div className={styles.container}>
+      <div className={styles.loginbox}>
+        <h1>Login</h1>
+        <form onSubmit={handleSubmit}>
+          <Input
+            required
+            type="email"
+            name="email"
+            value={formValue.email}
+            onChange={handleChange}
+            clearable
+            label="Email"
+            placeholder="Seu email...."
+            size="xl" width="100%"
+          />
+          <Input.Password
+            required
+            type="password"
+            name="password"
+            value={formValue.password}
+            onChange={handleChange}
+            label="Senha"
+            placeholder="Sua Senha...."
+            size="xl" width="100%"
+          />
 
-        <button type="submit" onClick={handleSubmit} disabled={loading}>{loading ? "Carregando..." : "Entrar"}</button>
+          <Spacer y={1}/>
+          <p> Não possui uma conta? <a href="../register"> Faça o cadastro.</a> </p>
+          <Spacer y={1}/>
+          <p style={{ color: 'red' }}>{error}</p>
+          <Spacer y={1}/>
+
+          <button type="submit" disabled={loading}>{loading ? "Carregando..." : "Entrar"}</button>
+        </form>
       </div>
     </div>
   );
